e571195ed2afef26c26a88343037a31e1a7d6738
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2018-01-30 00:01:14 +0100

    aggiunte alcune immagini creata la splash screen (capire perchè scritta copre 3/4 di schermo)

---
+++ app/src/main/java/com/xtini/mimalo/soundbuttongangedition/View/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.xtini.mimalo.soundbuttongangedition.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Teo on 29/01/2018.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        Intent intent = new Intent(this,StarChooserActivity.class);
+        startActivity(intent);
+    }
+}
