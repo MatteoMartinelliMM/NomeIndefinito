9cb604faf8828c8cdc6e70bb48f6888d4e688911
MatteoMartinelliMM <matteomartinelli1b@gmail.com>
2018-02-03 18:59:38 +0100

    avevo dimenticato il finish + modificato il git

---
+++ app/src/main/java/com/xtini/mimalo/soundbuttongangedition/View/TrapStarActivity.java
@@ -0,0 +1,16 @@
+package com.xtini.mimalo.soundbuttongangedition.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtini.mimalo.soundbuttongangedition.R;
+
+public class TrapStarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trap_star);
+
+    }
+}
