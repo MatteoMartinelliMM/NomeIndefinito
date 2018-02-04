e18eff117edd1f6dcfda37495ad5aed5260cdcfe
lorenzofoglia <lorenzo.foglia@studenti.unimi.it>
2018-02-04 03:00:58 +0100

    Implementato onClickListener sull'Artista per passare all'activity dei pulsanti

--- app/src/main/java/com/xtini/mimalo/soundbuttongangedition/Control/MyAdapter.java
+++ app/src/main/java/com/xtini/mimalo/soundbuttongangedition/Control/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.xtini.mimalo.soundbuttongangedition.Control;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.xtini.mimalo.soundbuttongangedition.R;
+import com.xtini.mimalo.soundbuttongangedition.View.StarChooserActivity;
+import com.xtini.mimalo.soundbuttongangedition.View.TrapStarActivity;
 
 import java.util.List;
 
@@ -48,6 +51,14 @@ public class MyAdapter extends PagerAdapter {
         ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
         imageView.setImageResource(lstImages.get(position));
         container.addView(view);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StarChooserActivity temp = (StarChooserActivity)v.getContext();
+                Intent intent = new Intent(temp, TrapStarActivity.class);
+                temp.startActivity(intent);
+            }
+        });
         return view;
     }
 }
